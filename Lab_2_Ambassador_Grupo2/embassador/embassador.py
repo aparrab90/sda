9354c20f09090bbe135171f5efe53eddab1384d5
aparrab90 <aparrab90@gmail.com>
2024-03-16 18:11:15 -0500

    Update embassador.py

--- Lab_2_Ambassador_Grupo2/embassador/embassador.py
+++ Lab_2_Ambassador_Grupo2/embassador/embassador.py
@@ -3,8 +3,7 @@ import requests
 
 app = Flask(__name__)
 
-# Direcciones de los servidores Flask subyacentes (shards)
-# Ajusta esta configuración para coincidir con tu entorno y configuración de docker-compose.yml
+
 servers = {
     'shard1': 'http://shard1:5000',
     'shard2': 'http://shard2:5000',
@@ -13,23 +12,23 @@ servers = {
 
 @app.route('/user/<shard_id>/<user_id>', methods=['GET'])
 def redirect_to_shard(shard_id, user_id):
-    # Verifica si el shard existe en la configuración de servidores
+    
     if shard_id not in servers:
         return jsonify({'error': f'Shard {shard_id} not found'}), 404
 
-    # Obtiene la dirección del servidor adecuado para el shard
+  
     server_url = servers[shard_id]
 
     try:
-        # Construye la URL del servidor destino y reenvía la solicitud
+      
         destination_url = f'{server_url}/user/{shard_id}/{user_id}'
         response = requests.get(destination_url)
 
-        # Retorna la respuesta recibida del servidor
+    
         return jsonify(response.json()), response.status_code
 
     except requests.exceptions.RequestException as e:
-        # Captura y maneja cualquier error de solicitud HTTP
+       
         return jsonify({'error': f'Error making HTTP request: {e}'}), 500
 
 if __name__ == '__main__':
