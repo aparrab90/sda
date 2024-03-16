48b65d99105c210ff6fb9fbfe25af80bcc76db69
Angel Javier Parra Bagua <aparrab90@gmail.com>
2024-03-16 13:27:45 -0500

    Labortorio2

---
+++ Lab_2_Ambassador_Grupo2/shard3/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, json
+
+app = Flask(__name__)
+
+# Carga los datos de los usuarios de datashard3.json
+with open('datashard3.json') as json_file:
+    users_data = json.load(json_file)
+
+@app.route('/user/<shard_id>/<user_id>')
+def get_user(shard_id, user_id):
+    # Verifica si el shard existe
+    if shard_id not in users_data:
+        return jsonify({'error': f'Shard {shard_id} not found'}), 404
+
+    shard = users_data[shard_id]
+
+    # Verifica si el usuario existe en el shard
+    if user_id not in shard:
+        return jsonify({'error': f'User {user_id} not found in shard {shard_id}'}), 404
+
+    user = shard[user_id]
+    return jsonify(user)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
